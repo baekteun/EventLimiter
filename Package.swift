c14e18c66d4d8c12b3be4fdd873417ed15e1fb5f
baegteun <baegteun@gmail.com>
2023-06-18 21:48:29 +0900

    :memo: :: Package.swift
    Platform 설정

--- Package.swift
+++ Package.swift
@@ -5,19 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "EventLimiter",
+    platforms: [.iOS(.v13), .tvOS(.v13), .macOS(.v10_15), .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "EventLimiter",
-            targets: ["EventLimiter"]),
+            targets: ["EventLimiter"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "EventLimiter"),
+            name: "EventLimiter"
+        ),
         .testTarget(
             name: "EventLimiterTests",
-            dependencies: ["EventLimiter"]),
+            dependencies: ["EventLimiter"]
+        ),
     ]
 )
